7eb9f3b7f437104ee00eed2cf747031b34b93bd4
jahid124 <jhasan576@gmail.com>
2022-05-10 19:23:43 +0600

    test

---
+++ week03/test.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define cl CLOCKS_PER_SEC
+#define pi acos(-1.0)
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define flw freopen ("0output.txt","w",stdout)
+#define flr freopen ("0input.txt","r",stdin)
+#define no1s(x) __builtin_popcount(x)
+#define parity(x) __builtin_parity(x)
+#define leadingZeros(x) __builtin_clz(x)
+#define trailingZeros(x) __builtin_ctz(x)
+#define mx 100005
+///mt19937 rng(chrono::steady_clock::now().time_since_epoch().count()); /// RANDOM SHUFFLE
+
+int main()
+{
+    int t,cs=1;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        vector<int> vc(n);
+        for(int i=0; i<n; i++) cin>>vc[i];
+        //sort(vc.begin(), vc.end());
+        for(int i=0; i<n; i++)
+        {
+
+        }
+
+    }
+    return 0;
+}
+
